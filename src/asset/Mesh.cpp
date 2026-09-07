7ed24be83d56c3d53863b1f6417a30a265d3d694
Kyota <132139102+KDKyota@users.noreply.github.com>
2026-09-08 03:09:26 +0900

    コメントを削除

--- src/asset/Mesh.cpp
+++ src/asset/Mesh.cpp
@@ -52,7 +52,6 @@ void Mesh::setupMesh() {
     glEnableVertexAttribArray(4);
     glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, stride, (void *)offsetof(gl::Vertex, bitangent));
 
-    // location 5 は使わない。
     // location 5 は使わない。point_shadow_depth.vert の aOffset がそこを読み、未有効化で既定値 (0,0,0) になる依存に合わせている
     glEnableVertexAttribArray(6);
     // 整数として渡すので I 付き。GL_INT を glVertexAttribPointer で送ると float に変換されて壊れる

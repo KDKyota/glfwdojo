f1098fe3bc2ee443b32729956a5f187c20213292
Kyota <132139102+KDKyota@users.noreply.github.com>
2026-09-22 19:17:55 +0900

    GeometryPass を Camera ではなく RenderView で駆動する
    鏡像カメラのように Camera の位置と姿勢で表せない視点からも描けるようにする
    clipPlane 引数も追加し 平面反射で床より上だけを残せるようにする 既定値では全ての断片を通す

--- src/render/pass/GeometryPass.h
+++ src/render/pass/GeometryPass.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "gl/Shader.h"
 #include "render/RenderSettings.h"
+#include "render/RenderView.h"
 #include "render/SceneGeometry.h"
 #include "render/targets/GBuffer.h"
-#include "scene/Camera.h"
 #include "scene/SceneModels.h"
 
 namespace gl {
@@ -20,9 +20,11 @@ class GeometryPass {
      *
      * @param heightScale Parallax Mapping の強さ
      * @param windowCount 並べ替え済みの窓の枚数
+     * @param clipPlane 断片を捨てる境界面 既定値は全ての断片を通す
      */
     void Execute(const GBuffer &gbuffer, const SceneGeometry &geometry, const SceneModels &models,
-                 const Camera &camera, const RenderSettings &settings, float heightScale, std::size_t windowCount);
+                 const RenderView &view, const RenderSettings &settings, float heightScale, std::size_t windowCount,
+                 const glm::vec4 &clipPlane = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
 
   private:
     Shader floorShader_;
